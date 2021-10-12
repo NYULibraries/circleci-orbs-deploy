4ff052c2a1c99a3e5272a0a7d4ff791a672b6a2e
BAlter <30699+barnabyalter@users.noreply.github.com>
2021-10-12 16:59:41 -0400

    Fixed complaints from yaml linter and shell linter

--- src/scripts/clear_old_builds.sh
+++ src/scripts/clear_old_builds.sh
@@ -1,17 +1,18 @@
-GetLatestVersions() {
+exclude_args=""
+get_latest_versions() {
   # Recursively list all lambda functions in bucket, sort by date, pull the latest N files, and extract just the filename into a temp file to maintain newline spacing
-  aws s3 ls s3://${S3_BUCKET}/${LAMBDA_FN}/ --recursive | sort | tail -n ${KEEP_BUILDS_NUM} | awk '{print $4}' > exclude.txt
+  aws s3 ls s3://"${S3_BUCKET}"/"${LAMBDA_FN}"/ --recursive | sort | tail -n "${KEEP_BUILDS_NUM}" | awk '{print $4}' > exclude.txt
 }
-GenerateExcludeArgs() {
-  exclude_args=""
+generate_exclude_args() {
   exclude_files="$(cat exclude.txt)"
   # Loop through files we want to keep and format as --exclude <filename> parameters for the AWS-CLI
-  for line in $exclude_files; do exclude_args="$exclude_args--exclude $(echo $line | awk -F'/' '{printf("%s/%s"), $2, $3}') "; done
-  return $exclude_args
+  for line in $exclude_files; do exclude_args="$exclude_args--exclude $(echo "$line" | awk -F'/' '{printf("%s/%s"), $2, $3}') "; done
 }
-DeleteS3Contents() {
+delete_s3_objects() {
   # Recursively remove all lambda functions of given name, in given bucket except the ones we explicitly exclude
-  aws s3 rm s3://${S3_BUCKET}/${LAMBDA_FN}/ --recursive $0
+  # shellcheck disable=SC2086
+  aws s3 rm s3://"${S3_BUCKET}"/"${LAMBDA_FN}"/ --recursive --dryrun $exclude_args
 }
-GetLatestVersions
-DeleteS3Contents GenerateExcludeArgs
+get_latest_versions
+generate_exclude_args
+delete_s3_objects
